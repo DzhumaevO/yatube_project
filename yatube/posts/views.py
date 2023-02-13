25a8319268b7e33ec3201c029ac9c55ad7606aa6
Орзу Джумаев <dzhumaevo@gmail.com>
2023-02-13 19:19:08 +0300

    для проврки, на работают группы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Лев Толстой – зеркало русской революции.'
+    context = {
+        'slug': slug,
+        'title': title,
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,10 +52,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+# anfisa/settings.py
+
+# Путь к директории с шаблонами вынесен в переменную:
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Добавлено: Искать шаблоны на уровне проекта
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,9 +68,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+            ]
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
@@ -119,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
