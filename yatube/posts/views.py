9687643c4ff5f9a147570436f867376e03fdf3c0
Орзу Джумаев <dzhumaevo@gmail.com>
2023-02-16 17:05:16 +0300

    перед сдачей

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -10,17 +10,18 @@ def index(request):
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
     # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date', 'group')[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
+        'title': 'Последние обновления на сайте',
     }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')[:10]
+    posts = group.posts.select_related('author', 'group')[:10]
     template = 'posts/group_list.html'
     context = {
         'group': group,
